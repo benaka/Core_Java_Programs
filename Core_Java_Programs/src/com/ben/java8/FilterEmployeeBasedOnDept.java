af37f597c80b61cc70690ac5d7e1a19d85078728
Benaka L Babu <benaka.lbabu@gmail.com>
2024-02-23 16:51:39 +0530

    Commiting employee based on dept java8

--- Core_Java_Programs/src/com/ben/java8/FilterEmployeeBasedOnDept.java
+++ Core_Java_Programs/src/com/ben/java8/FilterEmployeeBasedOnDept.java
@@ -11,12 +11,12 @@ public class FilterEmployeeBasedOnDept {
 	{
 		ArrayList<Employee> employeeDB = new ArrayList<Employee>();
 		
-		employeeDB.add(new Employee("Benaka",01,"Development","S/O LokeshBabu MR M N Kote Gubbi(Tq) Tumkur","100000"));
-		employeeDB.add(new Employee("Arun",02,"Development","No 48 Saraswathi puram Hydrabad","60000"));
-		employeeDB.add(new Employee("Lakkappa",03,"Development","No 89 laxmi puram Belgum","120000"));
-		employeeDB.add(new Employee("Meghana",04,"HR","No 60 halle market Mysore","80000"));
-		employeeDB.add(new Employee("Shivappa",05,"Manager","No 101 firozabad Hindupur","200000"));
-		employeeDB.add(new Employee("Harish",06,"IT","No 120 chanai Tamilnadu","176000"));
+		employeeDB.add(new Employee("Benaka",01,"Development","S/O LokeshBabu MR M N Kote Gubbi(Tq) Tumkur",(long) 100000));
+		employeeDB.add(new Employee("Arun",02,"Development","No 48 Saraswathi puram Hydrabad",(long)60000));
+		employeeDB.add(new Employee("Lakkappa",03,"Development","No 89 laxmi puram Belgum",(long)120000));
+		employeeDB.add(new Employee("Meghana",04,"HR","No 60 halle market Mysore",(long)80000));
+		employeeDB.add(new Employee("Shivappa",05,"Manager","No 101 firozabad Hindupur",(long)200000));
+		employeeDB.add(new Employee("Harish",06,"IT","No 120 chanai Tamilnadu",(long)176000));
 		
 		Map<String, List<Employee>> collect = employeeDB.stream().collect(Collectors.groupingBy(Employee::getDeptNo));
 		for(Entry e : collect.entrySet())
